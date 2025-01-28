f64fcb2612948bd1e4317b8f49562dfe0bf52047
ccem2163 <c.camilo85@gmail.com>
2025-01-28 15:51:12 -0500

    Primer ajuste de la página, 3 párrafos

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Página de Inicio en PHP</title>
+</head>
+<body>
+    <h1>¡Bienvenido a mi página de inicio en PHP!</h1>
+    <p>Esta es una página index sencilla creada con PHP.</p>
+
+    <?php
+        // Código PHP
+        echo "<p>¡Hola, este es un mensaje generado con PHP!</p>";
+    ?>
+</body>
+</html>
